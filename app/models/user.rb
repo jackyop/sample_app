17427a89f2683e55d13c2a9d67b7188007eb1087
carlo <carlor@uninorte.edu.co>
2016-04-24 10:50:14 -0500

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,14 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   #end of Lines for the hash password (se requiere user.authenticate(password) para acceder al password  )
 	before_save{ |user| user.email=email.downcase }
+	before_save :create_remember_token
 	validates(:name, presence: true,length:{maximum:50})
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates(:email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive:false})
 	#validates(:password, presence:true, length:{maximum:50})
 	#validates(:passsword_confirmation,presence:true)
+	private
+	def create_remember_token
+		self.remember_token = SecureRandom.urlsafe_base64
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy, :prueba]
   root to: 'static_pages#home'
+
+  get '/signin', to: 'sessions#new'
+  get '/signout', to: 'sessions#destroy', via: 'delete'
+
+  get '/prueba', to: 'sessions#prueba'
+
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
