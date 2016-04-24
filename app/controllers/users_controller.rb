96e256075810cdcea4ab72c63218cbff03e23cb8
carlo <carlor@uninorte.edu.co>
2016-04-24 16:47:28 -0500

    Finish user crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user, only: [:edit,:update]
+	before_filter :correct_user, only: [:edit,:update]
+	before_filter :admin_user, only: :destroy
 	def show
 		@user = User.find(params[:id])
 	end
@@ -14,5 +17,39 @@ class UsersController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+	def edit
+		@user = User.find(params[:id])
+	end
+	def update
+		@user = User.find(params[:id])
+		if @user.update_attributes(params[:user])
+			flash[:success] = "Profile updated"
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+	def index
+		@users = User.all
+	end
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed"
+		redirect_to users_path
+	end
+	private
+
+	def signed_in_user
+		store_location
+		redirect_to signin_path, notice: "Please Sign in" unless signed_in?
+	end
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_path) unless current_user?(@user)
+	end
+	def admin_user
+		redirect_to(root_path) unless current_user.admin?
 	end
 end
